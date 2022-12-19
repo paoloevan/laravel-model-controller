33c093745c518e666551d26d48dd5e1727d1d7bb
paoloevan <paoloev1900@gmail.com>
2022-12-19 15:26:28 +0100

    add cards

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row row-cols-5 g-5">
+            @foreach ($movies as $movie)
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="https://picsum.photos/200" alt="">
+                    <div class="card-body">
+                        <h3>
+                            {{$movie->title}}
+                        </h3>
+                        <div class="card-text">
+                            <p>{{$movie->nationality}}</p>
+                            <p>{{$movie->date}}</p>
+                            <p>{{$movie->vote}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+
+</body>
+
+</html>
